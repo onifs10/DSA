5ace43faf17e877825bb0abf2462c9e00757ed35
onifs10 <onifadebolu64@gmail.com>
2022-08-27 16:31:06 +0100

    Prefix accumulation 2D

--- src/Algorithm/PrefixAccumulation/__tests__/prefix_ac.test.ts
+++ src/Algorithm/PrefixAccumulation/__tests__/prefix_ac.test.ts
@@ -1,7 +1,7 @@
-import type {Reducer, Container} from "../index";
-import  {inplacePrefixAcc1D, prefixAcc1D} from "../index";
+import type {Reducer, Container, Container2D} from "../index";
+import  {inplacePrefixAcc1D, prefixAcc1D, inplacePrefixAcc2D,prefixAcc2D,default2dReducer} from "../index";
 
-describe("Prefix accumulation" , () => {
+describe("Prefix accumulation 1D" , () => {
     it('test inplace accumulation', () => {
         const reducer: Reducer<number>  = (prefixSum, item) => prefixSum + item;
         const list: Container<number> = [1,0,4,6,-2];
@@ -14,5 +14,20 @@ describe("Prefix accumulation" , () => {
         const list: Container<number> = [1,0,4,6,-2];
         const result = prefixAcc1D(list, reducer);
         expect(result).toEqual([1,1,5,11,9]);
+    })
+})
+
+describe("Prefix accumulation 2D" , () => {
+    
+    it('test inplace accumulation', () => {
+        const matrix : Container2D<number> = [[1,0,1],[0,-2,3]];
+        inplacePrefixAcc2D(matrix, default2dReducer);
+        expect(matrix).toEqual([ [ 1, 1, 2 ], [ 1, -1, 3 ] ]);
+    })
+
+    it('test accumulation' , () => {
+        const matrix : Container2D<number> = [[1,0,1],[0,-2,3]];
+        const matrixSum = prefixAcc2D(matrix, default2dReducer);
+        expect(matrixSum).toEqual([ [ 1, 1, 2 ], [ 1, -1, 3 ] ]);
     })
 })
--- src/Algorithm/PrefixAccumulation/index.ts
+++ src/Algorithm/PrefixAccumulation/index.ts
@@ -11,6 +11,7 @@
  * a container with a contigious set of data;
  */
 export type Container<T> = Array<T> 
+export type Container2D<T> = Container<Container<T>>;
 
 /**
  * a reducer interface that handles the acccumulation logic 
@@ -19,9 +20,18 @@ export interface Reducer<T> {
     (prefixAcc: T, indexValue:T) : T;  
 }
 
+/** 
+ * reducer interface for 2D containers 
+ * param // coordinate of postion to evaluate 
+ *     x : number
+ *     y : number 
+ */
+export interface Reducer2D<T> {
+    (x: number, y:number, matrix: Container2D<T>) : T;  
+}
 
 /**
- * inplace prefix accumulation
+ * inplace prefix accumulationc1D
  * space complexity O(1)
  * time complexity O(n)
  */
@@ -33,9 +43,12 @@ export const inplacePrefixAcc1D = <T>(arr: Container<T>, reducer:Reducer<T> ) : 
 
 
 /**
- * prefix accumulation
+ * prefix accumulation 1D
  * space complexity O(n)
  * time complexity O(n)
+ * @param arr Container
+ * @param reducer Reducer
+ * @returns Container
  */
 export const prefixAcc1D = <T>(arr: Container<T>, reducer:Reducer<T> ): Container<T> => {
     const result : T[] =  [arr[0]]
@@ -44,3 +57,48 @@ export const prefixAcc1D = <T>(arr: Container<T>, reducer:Reducer<T> ): Containe
     }
     return result;
 }
+
+/**
+ * reducer for 2D container with type number
+ * @param x number
+ * @param y number
+ * @param matrix Container2D<number>
+ * @returns number
+ */
+export const default2dReducer = (x : number, y : number, matrix : Container2D<number>): number  => { 
+    let top : number = matrix[x - 1]?.[y] ?? 0;
+    let left : number = matrix[x][y - 1] ?? 0;
+    let digonalLeft : number= matrix[x - 1]?.[y -1] ?? 0;
+    return matrix[x][y] + top + left - digonalLeft;
+}
+
+
+/**
+ * inplace prefix accumlation 2D 
+ * space complexity O(1);
+ * timeComplexity O(mn)
+ * @param matrix Container2D
+ * @param reducer Reducer2D
+ */
+ export const inplacePrefixAcc2D = <T>(matrix: Container2D<T>, reducer:Reducer2D<T>) : void  => {
+    for (let x = 0; x < matrix.length; x++){
+        for(let y = 0; y < matrix[x].length; y++){
+            matrix[x][y] = reducer(x, y, matrix);
+        }
+    }
+}
+
+export const prefixAcc2D = <T>(matrix: Container2D<T>, reducer:Reducer2D<T>) : Container2D<T>  => {
+    let outMatrix: Container2D<T> = [];
+
+    for (let i = 0; i < matrix.length; i++){
+        outMatrix.push([...matrix[i]]);
+    }
+    for (let x = 0; x < matrix.length; x++){
+        for(let y = 0; y < matrix[x].length; y++){
+            outMatrix[x][y] = reducer(x, y, outMatrix);
+        }
+    }
+    return outMatrix;
+}
+
